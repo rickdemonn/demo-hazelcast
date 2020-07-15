228aa5ff73493c8ddf17df9d773675bbfc75334a
Dima Denysko <iguanastarka@gmail.com>
2020-07-15 17:29:50 +0300

    Enable time to live

--- src/main/java/com/example/demohazelcast/model/Book.java
+++ src/main/java/com/example/demohazelcast/model/Book.java
@@ -5,13 +5,14 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.io.Serializable;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
 @Table(name = "books")
-public class Book {
+public class Book implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
