62ba2bede29a2048c2c4cc05d1f73e18cee035e9
Dima Denysko <iguanastarka@gmail.com>
2020-07-16 11:02:59 +0300

    CacheEvict and CachePut

--- src/main/java/com/example/demohazelcast/HazelcastConfiguration.java
+++ src/main/java/com/example/demohazelcast/HazelcastConfiguration.java
@@ -1,7 +1,9 @@
 package com.example.demohazelcast;
 
 import com.hazelcast.config.Config;
+import com.hazelcast.config.EvictionPolicy;
 import com.hazelcast.config.MapConfig;
+import com.hazelcast.config.MaxSizeConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,11 +13,12 @@ public class HazelcastConfiguration {
     @Bean
     public Config hazelCastConfig(){
         return new Config()
-                .setClusterName("My-Cluster")
                 .setInstanceName("hazelcast-instance")
                 .addMapConfig(
                         new MapConfig()
                                 .setName("books")
+                                .setMaxSizeConfig(new MaxSizeConfig(200, MaxSizeConfig.MaxSizePolicy.FREE_HEAP_SIZE))
+                                .setEvictionPolicy(EvictionPolicy.LRU)
                                 .setTimeToLiveSeconds(10));
     }
 
--- src/main/java/com/example/demohazelcast/service/HazelCastService.java
+++ src/main/java/com/example/demohazelcast/service/HazelCastService.java
@@ -5,6 +5,8 @@ import com.example.demohazelcast.model.Book;
 import com.example.demohazelcast.repo.HazelCastRepo;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -20,14 +22,20 @@ public class HazelCastService {
         this.hazelCastRepo = hazelCastRepo;
     }
 
+    /*
+    when book added or deleted getOne works from cache
+     */
     @SneakyThrows
-    @Cacheable("books")
+    @Cacheable(value = "books", key = "#id")
     public Book getOne(Integer id) {
         log.info("get one");
         Thread.sleep(2000);
         return hazelCastRepo.findById(id).orElseThrow(BookNotFoundException::new);
     }
 
+    /*
+    Do not updating cache when book added or deleted гтешд scheduling starts
+     */
     @SneakyThrows
     @Cacheable("books")
     @Scheduled(fixedRate = 8000)
@@ -37,10 +45,12 @@ public class HazelCastService {
         return hazelCastRepo.findAll();
     }
 
+    @CachePut(value = "books", key = "#book.id")
     public Book create(Book book) {
         return hazelCastRepo.save(book);
     }
 
+    @CachePut("books")
     public Book update(Integer id, Book book) {
         Book bookFromDb = hazelCastRepo.findById(id).orElseThrow(BookNotFoundException::new);
         bookFromDb.setTitle(book.getTitle());
@@ -48,6 +58,7 @@ public class HazelCastService {
         return hazelCastRepo.save(bookFromDb);
     }
 
+    @CacheEvict(value = "books", key = "#id")
     public void delete(Integer id) {
         hazelCastRepo.deleteById(id);
     }
